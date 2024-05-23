dbf4ca328d5f5c24b35c2baced2f0e229b9b75c8
Koushik Ahmad <koushikahmad123@gmail.com>
2024-05-23 12:48:49 +0600

    part-9

---
+++ src/app/modules/Schedule/schedule.routes.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import { ScheduleController } from "./schedule.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+
+const router = express.Router();
+
+router.get("/", auth(UserRole.DOCTOR), ScheduleController.getAllFromDB);
+
+/**
+ * API ENDPOINT: /schedule/:id
+ *
+ * Get schedule data by id
+ */
+
+router.get(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+  ScheduleController.getByIdFromDB
+);
+
+router.post(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  ScheduleController.insertIntoDB
+);
+
+/**
+ * API ENDPOINT: /schedule/:id
+ *
+ * Delete schedule data by id
+ */
+
+router.delete(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  ScheduleController.deleteFromDB
+);
+
+export const ScheduleRoutes = router;
